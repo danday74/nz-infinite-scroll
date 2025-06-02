964f135dd8371d87284c7c11e4c152c70fee3ae7
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-06-02 14:32:20 +0100

    refactor

--- src/app/components/my-component/my-component.component.ts
+++ src/app/components/my-component/my-component.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core'
-import { NzInfiniteScrollComponent } from '../nz-infinite-scroll/nz-infinite-scroll.component'
 
 @Component({
   selector: 'app-my-component',
   standalone: true,
-  imports: [
-    NzInfiniteScrollComponent,
-  ],
+  imports: [],
   templateUrl: './my-component.component.html',
   styleUrl: './my-component.component.scss',
 })
